cac2ec6beceabb1f1686439bf22a5bd3de19e602
keviljh3 <kevinljh3@hotmail.com>
2018-09-26 03:04:17 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,11 +3,11 @@ killall udp2raw_amd64
 killall python                                                                                                  
 killall node
 ulimit -n 65535
-nohup ./server_linux_amd64 -t "0.0.0.0:17520" -l ":17518" -key test -mtu 1350 -sndwnd 610 -rcvwnd 610 -crypt none -mode fast2 -dscp 0 -datashard 10 -parityshard 3 -nocomp> kcptun.log 2>&1 &
+nohup ./kcps64_170120 -t "0.0.0.0:17520" -l ":17518" -key test -mtu 1350 -sndwnd 1024 -rcvwnd 1024 -crypt none -mode fast2 -dscp 0 -datashard 0 -parityshard 0 -nocomp> kcptun.log 2>&1 &
 nohup ./udp2raw_amd64 -s -l0.0.0.0:17517 -r 127.0.0.1:17518 -k "passwd" --raw-mode faketcp -a > udp.log 2>&1 &
 nohup node html.js > nodejs.log 2>&1 &
 nohup ./nrop_linux_amd64 > nrop.log 2>&1 &
 cd /fs
 sh restart.sh
-nohup python /shadowsocksr-akkariiin-dev/shadowsocks/server.py -p 17520 -k Ssr123456 -m chacha20-ietf -O auth_chain_e -o tls1.2_ticket_auth -G 32 -g www.bing.com > ssr.log 2>&1 &
+nohup python /shadowsocksr-akkariiin-dev/shadowsocks/server.py -p 17520 -k Ssr123456 -m rc4 -O auth_chain_e -o plain > ssr.log 2>&1 &
 cd /shadowsocksr-akkariiin-dev/shadowsocks
